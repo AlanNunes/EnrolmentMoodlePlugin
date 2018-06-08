49df1644b49c64508c3c755126712ab33be75918
AlanNunes <alann.625@gmail.com>
2018-06-08 18:51:32 -0300

    Cria o script para alertar alunos que estão prestes a terminar o módulo

---
+++ php/enrolments/Alerts.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created on 08/06/2018
+ * Script responsável por dar alertas aos estudantes, por exemplo: o aluno está prestes a terminar o curso
+ *
+ * @category   Enrolments
+ * @author     Alan Nunes da Silva <alann.625@gmail.com>
+ * @author     Gustavo de Mello Brandão <sm70plus@gmail.com>
+ * @copyright  2018 Dual Dev
+ */
+
+include_once('Enrolments.php');
+include_once('../grades/Grades.php');
+include_once('../mails/Outlook_Mails.php');
+
+}
+
+?>
--- php/enrolments/Controller.php
+++ php/enrolments/Controller.php
@@ -2,7 +2,7 @@
 /**
  * Created on ??/04/2018
  *
- * @category   Grades
+ * @category   Enrolments
  * @author     Alan Nunes da Silva <alann.625@gmail.com>
  * @author     Gustavo de Mello Brandão <sm70plus@gmail.com>
  * @copyright  2018 Dual Dev
