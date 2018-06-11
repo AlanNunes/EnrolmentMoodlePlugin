556dfe9f20252e5665eba9537dfe8509642c6c8c
AlanNunes <alann.625@gmail.com>
2018-06-11 01:10:51 -0300

    Comenta o código do Controller.php de Enrolments e deixa o mesmo legível a outros desenvolvedores

--- php/enrolments/Controller.php
+++ php/enrolments/Controller.php
@@ -15,98 +15,231 @@ include_once('../grades/Grades.php');
 include_once('../modulos/Modulos.php');
 include_once('../mails/Outlook_Mails.php');
 
-// Connect to the External Database
+/**
+* DataBase Connection
+*
+* Connect to the external database
+*/
 $dbExternal = new DataBase("external_enrolment");
 $connExternal = $dbExternal->getConnection();
 
-// Connect to the Moodle
+/**
+* DataBase Connection
+*
+* Connect to moodle database
+*/
 $dbMoodle = new DataBase("moodle");
 $connMoodle = $dbMoodle->getConnection();
 
-// Create an instance for Enrolments
+/**
+* Enrolments
+*
+* Create an instance for Enrolments
+*/
 $enrolments = new Enrolments($connMoodle);
+/**
+* Enrolments
+*
+* Create an instance for Enrolments from Graduação
+*/
 $enrolmentsGraduacao = new EnrolmentsGraduacao($connMoodle);
+/**
+* Enrolments
+*
+* Create an instance for Enrolments from Pós-Graduação
+*/
 $enrolmentsPosGraduacao = new EnrolmentsPosGraduacao($connMoodle);
-// Cria uma instância da Classe Grades(matrizes)
+/**
+* Grades
+*
+* Create an instance for Grades(matrizes)
+*/
 $grades = new Grades($connExternal);
-// Create an instance of Modulos with connection to external database
+/**
+* Módulos
+*
+* Create an instance for Módulos
+*/
 $modulos = new Modulos($connExternal);
 
-// Get all the students that are not enrolled in any course from POS-EAD
+/**
+* Enrolments
+*
+* @var array students not subscribed in any course
+* Get all the students that are not enrolled in any course from POS-EAD
+* The response is an array containing
+*/
 $studentsNotEnrolledsPosGrad = $enrolmentsPosGraduacao->getStudentsNotSubscribedInAnyCourse();
-// TDD
-var_dump($studentsNotEnrolledsPosGrad);
+
+var_dump($studentsNotEnrolledsPosGrad); // TDD
+
+/**
+* Check if exists students not subscribed in any course
+*/
 if($studentsNotEnrolledsPosGrad['students']){
   foreach($studentsNotEnrolledsPosGrad['students'] as $student){
-    // Get basic information of the student
     $username = $student['username'];
+    /**
+    * Save student's data in '$city' and '$course'
+    * It assumes that the student's lastnme has this default: 'CITY-COURSE'
+    * Example: 'VR-SIS'
+    */
     list($city, $course) = explode("-", $student['lastname']);
-    // End getting basic information
-    // Matricula aluno da Pós EAD no primeiro módulo, da matriz ativa atual
+    /**
+    * Enrolments
+    *
+    * Matricula aluno da Pós EAD no primeiro módulo, da matriz ativa atual
+    *
+    * @param array $student Array structure that contains student basic info
+    * @param string $course Contains the course's shortname, example 'SIS'
+    */
     matriculaAlunoPosEAD($student, $course);
   }
 }
 
-// *************************************************************************************************
+// ****************************************************************************
 
-// Get all the students that are not enrolled in any course from POS-EAD
+/**
+* Enrolments
+*
+* @var array students not subscribed in any course
+* Get all the students that are not enrolled in any course from Graduação
+* The response is an array containing
+*/
 $studentsNotEnrolledsGraduacao = $enrolmentsGraduacao->getStudentsNotSubscribedInAnyCourse();
-// TDD
-var_dump($studentsNotEnrolledsGraduacao);
+
+var_dump($studentsNotEnrolledsGraduacao); // TDD
+
+/**
+* Check if exists students not subscribed in any course
+*/
 if($studentsNotEnrolledsGraduacao['students']){
   foreach($studentsNotEnrolledsGraduacao['students'] as $student){
-    // Get basic information of the student
     $username = $student['username'];
+    /**
+    * As moodle creates a default user called 'guest' with a blank space in it's
+    * lastname, this conditional checks for it, and prevent erros
+    */
     if($student['lastname'] != ' '){
+      /**
+      * Save student's data in '$city', '$course', '$horario', '$tipo', '$periodo'
+      * It assumes that the student's lastnme has this
+      * default: 'CITY-COURSE-SCHEDULE-TYPE-PERIODO'
+      * Example: 'VR-SIS-N18.1-REG-1PER'
+      */
       list($city, $course, $horario, $tipo, $periodo) = explode("-", $student['lastname']);
+      // Pega somente o número do período
       $periodo = preg_replace("/[^0-9]/", '', $periodo);
-      // Matricula aluno da Pós EAD no primeiro módulo, da matriz ativa atual
+      /**
+      * Enrolments
+      *
+      * Matricula aluno da Pós EAD no primeiro módulo, da matriz ativa atual
+      *
+      * @param array $student Array structure that contains student basic info
+      * @param string $course Contains the course's shortname, example 'SIS'
+      * @param integer $periodo It has the período number
+      */
       matriculaAlunoGraduacao($student, $course, $periodo);
     }
   }
 }
 
-// Create an instance for students of Pós-Graduação
-// $enrolmentsPosGraduacao = new EnrolmentsPosGraduacao($connExternal);
+/**
+* Change connection's to external database
+*/
 $enrolmentsPosGraduacao->conn = $connExternal;
+/**
+* Gets all the students that has the time expired by the paramater
+* @param integer Limit time of enrolment
+*/
 $studentsPosGradExpiredTime = $enrolmentsPosGraduacao->getStudentsByTimeEnrolment(60*1);
-echo '<p>Estudantes da Pós-Graduação com mais de {60*1} segundos inscritos num curso</p>';
-var_dump($studentsPosGradExpiredTime);
+echo '<p>Estudantes da Pós-Graduação com mais de {60*1} segundos inscritos num curso</p>'; // TDD
+var_dump($studentsPosGradExpiredTime); // TDD
 
-// Just go into the IF scope if there is some students not enrolled in any course
+/**
+* It checks if there's any student at limit time of enrolment
+*/
 if($studentsPosGradExpiredTime['students']){
   foreach($studentsPosGradExpiredTime['students'] as $student){
     // It switches the database connection to 'External Database'
     $enrolments->conn = $GLOBALS['connExternal'];
-    // It switches the database connection to 'External Database'
-    // $enrolments->conn = $connExternal;
-    echo "<h1>shortnamecourse: ".$student['shortnamecourse']."</h1>";
+    echo "<h1>shortnamecourse: ".$student['shortnamecourse']."</h1>"; // TDD
+    /**
+    * Enrolments
+    *
+    * Enrol the student in the next course(módulo)
+    * @param string username Student's username
+    * @param string shortnamecourse Course's shortname
+    * @param integer matriz Matriz(Grade) to be enrolled
+    * @return array It returns a colletions of information about the process
+    */
     $enrolmentsPosGraduacao->enrolInNextCourse($student['username'], $student['shortnamecourse'], $student['matriz']);
+    /**
+    * Execute the sync
+    */
     exec('php C:\wamp\www\moodle\enrol\database\cli\sync.php');
   }
 }
 
+/**
+* Enrolments
+*
+* Enrols the student from Pós-Graduação in a course
+* @param array $student Basic student information
+* @param integer $course Id of the course to be enrolled
+*/
 function matriculaAlunoPosEAD($student, $course){
-  // Create an instance for Enrolments
+  /**
+  * Enrolments
+  *
+  * Create an instance for Enrolments
+  */
   $enrolments = new Enrolments($GLOBALS['connMoodle']);
-  // Cria uma instância da Classe Grades(matrizes)
+  /**
+  * Grades - Matrizes
+  *
+  * Create an instance for Grades(matrizes)
+  */
   $grades = new Grades($GLOBALS['connExternal']);
-  // Create an instance of Modulos with connection to external database
+  /**
+  * Módulos - Courses
+  *
+  * Create an instance for Módulos(courses)
+  */
   $modulos = new Modulos($GLOBALS['connExternal']);
 
-  echo "<h1>Aluno é da Pós-EaD:</h1> <h3>{$student['username']}</h3>";
+  echo "<h1>Aluno é da Pós-EaD:</h1> <h3>{$student['username']}</h3>"; // TDD
   $username = $student['username'];
+  /**
+  * Get student firstname
+  * It assumes that the student firstname has the default: Firstname Surname
+  */
   list($firstname) = explode(" ", $student['firstname']);
   $email = $student['email'];
+  /**
+  * Grades
+  *
+  * Pega o id da matriz de acordo com o curso e a modalidade
+  * @param string $course Shortname do curso
+  * @param string Modalidade do curso
+  * @return array Returns a colletion of description about the process
+  */
   $matrizResponse = $grades->getMatrizByCourseAndModalidade($course, 'EAD');
   if(!$matrizResponse["erro"]){
     $matriz = $matrizResponse['matriz']; // Pega a matriz que o aluno deve ser inscrito na matricula
-    // Pega o 'shortname' do curso que aparece em primeiro na lista da matriz
-    $modulosResponse = $modulos->getShortNameCourseByGrade($matriz);
+    $modulosResponse = $modulos->getShortNameCourseByGrade($matriz); // Pega o 'shortname' do curso que aparece em primeiro na lista da matriz
     $shortnamecourse = $modulosResponse["shortnamecourse"];
     // It switches the connection of enrolments from moodle to the external database's connection
     $enrolments->conn = $GLOBALS['connExternal'];
-    // Enroll the student the course and catch the response of it's process
+    /**
+    * Enrolments
+    *
+    * Enroll the student in the course - módulo
+    * @param string $username Student username
+    * @param string $shortnamecourse Course shortname
+    * @param integer $matriz Grade's id - Id da matriz
+    * @return array Returns a colletion of description about the process
+    */
     $enrolmentResponse = $enrolments->enrolStudentInCourse($username, $shortnamecourse, $matriz);
     if(!$enrolmentResponse['erro']){
       // Envia e-mail
@@ -121,7 +254,7 @@ function matriculaAlunoPosEAD($student, $course){
     }
     // Execute the sync.php
     exec('php C:\wamp\www\moodle\enrol\database\cli\sync.php');
-    echo json_encode($enrolmentResponse);
+    echo json_encode($enrolmentResponse); // TDD
   }else{
     // Houve algum erro ao buscar a matriz
     // save in log the error
